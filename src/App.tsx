34cca03b9154044f97f0461e00fa1ae902b3d9e1
mardg09 <153201150+mardg09@users.noreply.github.com>
2024-12-27 10:23:06 +0800

    Back to Default

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Mar Vincent O. De Guzman</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Fuck <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">FUCKFUCKFUCKFUCK</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
